c1d88a41dec3795e887a8f0d39d74bc41d505901
Buddy <bgor010@aucklanduni.ac.nz>
2015-03-05 17:35:11 +1300

    Added Bootstrap and "/game" path.

--- life/src/main/java/life/controllers/CellController.java
+++ life/src/main/java/life/controllers/CellController.java
@@ -11,16 +11,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/board")
 public class CellController {
 	
 	@Autowired Game game;
 	
-	@RequestMapping(name="/board", method={RequestMethod.GET})
+	@RequestMapping(method={RequestMethod.GET})
 	public List<life.models.CellModel> getCells(int x, int y, int width, int height, int generation){
 		return game.getCells(x, y, width, height, generation);
 	}
 	
-	@RequestMapping(name="/board", method={RequestMethod.POST})
+	@RequestMapping(method={RequestMethod.POST})
 	public boolean setCells(@ModelAttribute List<life.models.CellModel> cells, int generation) {
 		return game.setCells(cells, generation);
 	}
